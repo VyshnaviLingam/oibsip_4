4d17bb189fb610086baffd0b2daa7d19ff488162
VyshnaviLingam <145998873+VyshnaviLingam@users.noreply.github.com>
2023-10-06 00:25:47 +0530

    Add files via upload

---
+++ calculator.css
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+body {
+    background-color:blanchedalmond;
+            margin: 20%;;
+    }
+h1#title{
+    color: rgba(255, 166, 0, 0.89);
+}
+#title {
+    text-align: center;
+    text-shadow: 5px 5px 10px black ;
+    font-size: 7vh;
+}
+img {
+    display: inline-block;
+    width: 100%;
+    padding: 25px 25px;
+    border:20px solid black;
+}
+#caption {
+    font-size: 25px;
+    font-style: inherit;
+    font-family: Gill Sans;
+    color: blue;
+}
+div#tribute-data {
+    background-color:rgb(46, 139, 87, 0.25);
+    box-shadow: 20px 20px 20px black;
+    font-family: Georgia;
+    padding: 25px 25px;
+    margin: 11px;
+    margin-top: 50px;
+}
+h1.capt {
+    font-size: 35px;
+    color: white;
+    text-align: center;
+    text-shadow: 5px 5px 10px black;
+}
+</style>
+</head>
+<body>
+<main id="main">
+<h1 id="title">
+   Mahatma Gandhi
+</h1>
+<div id="img">
+    <img src="https://tse1.mm.bing.net/th?id=OIP.FXqOCHlYMqv5zoLBscM7iAHaFS&pid=Api&P=0&h=180"
+         id="image" alt="Error ">
+    <bold id="caption">  Well-known as a freedom fighter and the Father of our Nation, Indian Lawyer ,Politican, Social Activist and Writer
+</bold>
+
+</div>
+<div id="tribute-data">
+    <h1 class="capt">
+        Father of Our Nation 
+    </h1>
+    <p>
+        ☛ Mahatma Gandhi, byname of Mohandas Karamchand Gandhi, (born October 2, 1869, Porbandar, India—died January 30, 1948, Delhi), Indian lawyer, politician, social activist, and writer who became the leader of the nationalist movement against the British rule of India. <br><br>
+        ☛ Born and raised in a Hindu family in coastal Gujarat, Gandhi trained in the law at the Inner Temple, London, and was called to the bar at age 22 in June 1891. After two uncertain years in India, where he was unable to start a successful law practice, he moved to South Africa in 1893 to represent an Indian merchant in a lawsuit. He went on to live in South Africa for 21 years. <br><br>
+        ☛ It was here that Gandhi raised a family and first employed nonviolent resistance in a campaign for civil rights. In 1915, aged 45, he returned to India and soon set about organising peasants, farmers, and urban labourers to protest against excessive land-tax and discrimination. <br> <br>
+        ☛ Assuming leadership of the Indian National Congress in 1921, Gandhi led nationwide campaigns for easing poverty, expanding women's rights, building religious and ethnic amity, ending untouchability, and, above all, achieving swaraj or self-rule. Gandhi adopted the short dhoti woven with hand-spun yarn as a mark of identification with India's rural poor. He began to live in a self-sufficient residential community, to eat simple food, and undertake long fasts as a means of both introspection and political protest. Bringing anti-colonial nationalism to the common Indians, Gandhi led them in challenging the British-imposed salt tax with the 400 km (250 mi) Dandi Salt March in 1930 and in calling for the British to quit India in 1942. He was imprisoned many times and for many years in both South Africa and India.<br><br>
+        ☛ Gandhi's vision of an independent India based on religious pluralism was challenged in the early 1940s by a Muslim nationalism which demanded a separate homeland for Muslims within British India. In August 1947, Britain granted independence, but the British Indian Empire was partitioned into two dominions, a Hindu-majority India and a Muslim-majority Pakistan. <br><br>
+        ☛ As many displaced Hindus, Muslims, and Sikhs made their way to their new lands, religious violence broke out, especially in the Punjab and Bengal. Abstaining from the official celebration of independence, Gandhi visited the affected areas, attempting to alleviate distress. In the months following, he undertook several hunger strikes to stop the religious violence. The last of these was begun in Delhi on 12 January 1948 when he was 78.<br><br>
+        ☛ The belief that Gandhi had been too resolute in his defense of both Pakistan and Indian Muslims spread among some Hindus in India. <br><br>
+        ☛ The belief that Gandhi had been too resolute in his defense of both Pakistan and Indian Muslims spread among some Hindus in India. <br><br>
+        ☛ Among these was Nathuram Godse, a militant Hindu nationalist from Pune, western India, who assassinated Gandhi by firing three bullets into his chest at an interfaith prayer meeting in Delhi on 30 January 1948.<br><br>
+        ☛ Gandhi's birthday, 2 October, is commemorated in India as Gandhi Jayanti, a national holiday, and worldwide as the International Day of Nonviolence. Gandhi is considered to be the Father of the Nation in post-colonial India. During India's nationalist movement and in several decades immediately after, he was also commonly called Bapu.<br><br>
+        ☛ As such, he came to be considered the father of his country. Gandhi is internationally esteemed for his doctrine of nonviolent protest (satyagraha) to achieve political and social progress.<br><br>
+    </p>
+</div>
+<br>
+</main>
+</body>
+</html>
